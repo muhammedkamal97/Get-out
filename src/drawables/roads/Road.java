2d8a0d3876c8ca9ae4fff09be89f301edd2611c1
Aya <ayasaberosman@gmail.com>
2017-12-06 11:06:53 +0200

    Add unimplemented method to class Road

--- src/drawables/roads/Road.java
+++ src/drawables/roads/Road.java
@@ -1,5 +1,7 @@
 package drawables.roads;
 
+import java.awt.Point;
+
 import drawables.Drawable;
 import javafx.scene.canvas.Canvas;
 
@@ -11,5 +13,17 @@ public class Road implements Drawable {
         //get the road image and put on canvas
 
     }
+
+	@Override
+	public Point getPosition() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setPosition(Point position) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
