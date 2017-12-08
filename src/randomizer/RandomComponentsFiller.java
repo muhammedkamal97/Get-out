31a4562b544fe6116ec12fcc8466e04e74f74c97
abdelrahman <abdelrahman.ibrahim.009@gmail.com>
2017-12-08 17:13:26 +0200

    random generator

---
+++ src/randomizer/RandomComponentsFiller.java
@@ -0,0 +1,36 @@
+package randomizer;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class RandomComponentsFiller {
+
+    private Random random = new Random();
+
+    //add cloneable.
+
+    public Object generateRandomArray(
+            Class[] referenceArray,
+            int maxNumberAllowed
+                                 ){
+        int enteredArraySize = referenceArray.length;
+        ArrayList<Object> randomArray = new ArrayList<>();
+        for(int i = 0 ;i < maxNumberAllowed ; i++){
+            try {
+                randomArray.
+                        add(referenceArray[generateRandomNumber(enteredArraySize)]
+                                .getConstructor().newInstance());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return randomArray;
+    }
+
+
+    private int generateRandomNumber(int limit){
+        return random.nextInt(limit);
+    }
+
+}
--- src/randomizer/RandomNumbersGenerator.java
+++
@@ -1,4 +0,0 @@
-package randomizer;
-
-public class RandomNumbersGenerator {
-}
